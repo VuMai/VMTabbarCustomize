180aa62e0396e6ea5dcd494b44a2880323ee4049
VuMai <vumai.mlh@gmail.com>
2015-04-21 19:50:26 +0700

    Initial Commit

---
+++ VMTabbarCustomize/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  VMTabbarCustomize
+//
+//  Created by Vu Mai on 4/21/15.
+//  Copyright (c) 2015 VuMai. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ VMTabbarCustomize/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  VMTabbarCustomize
+//
+//  Created by Vu Mai on 4/21/15.
+//  Copyright (c) 2015 VuMai. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
